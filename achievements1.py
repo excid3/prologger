a837a6430b629d7bd4a84e0422f444f1d14b6c73
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2011-03-12 14:11:27 -0500

    fixing typo

--- achievements1.py
+++ achievements1.py
@@ -22,7 +22,7 @@ def string_found(string1, string2):
 def main():
 	username = sys.argv[1]
 	api_token = sys.argv[2]
-	ach	= Achievements(username, api_token)
+	ach	= Achievements1(username, api_token)
 	points = ach.get_achievements()
 	print "You have unlocked the following achievements! Congratulations!!"
 	print simplejson.dumps(points)
