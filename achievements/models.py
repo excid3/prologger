90707a03b13ee3ce274d38ee4e9eaf91f2ef3ecf
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2011-04-06 00:24:48 -0400

    adding requirements to file

--- achievements/models.py
+++ achievements/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 
@@ -9,8 +10,11 @@ class ProloggerUserManager(models.Manager):
 		pro = ProloggerUser(user = user)
 		pro.save()
 		
-class AchievementsManager(models.Manager):
-	pass
+#class AchievementsManager(models.Manager):
+#	def create_achievement(self, name, date, description, points = None):
+#	    achievement = Achievements(name = name, date = date, description = description, points = points)
+#	    achievement.save()
+        
 	
 		
 class Achievements(models.Model):
@@ -18,9 +22,11 @@ class Achievements(models.Model):
 	points = models.IntegerField(blank=True, null = True)
 	date = models.DateField(auto_now_add=True)
 	description = models.CharField(max_length = 140)
-	objects = AchievementsManager()	
 	def __unicode__(self):
 		return self.name
+		
+	class Meta:
+	    verbose_name_plural = "Achievements"
 	
 class ProloggerUser(models.Model):
 	user = models.ForeignKey(User, unique=True)
@@ -31,3 +37,5 @@ class ProloggerUser(models.Model):
 	
 	def __unicode__(self):
 		return u"Prologger information for %s" % self.user
+	class Meta:
+	    verbose_name_plural = "ProloggerUsers"
