ce2e7d9af4dc08cd8f1abe3ba0904acdfa51219e
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2011-04-14 20:50:56 -0400

    adding github auth logo fixed view issue -ish

--- urls.py
+++ urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.defaults import *
 # import in order to access settings.py variables
 from django.conf import settings
 
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+
 # importing all the views  
 from views import *
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -17,9 +18,9 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
      (r'^admin/doc/', include('django.contrib.admindocs.urls')),
      url(r'^$', 'views.view', {'template': 'index.html'}, name='index'),
-     (r'^login/', login_),
-     (r'^logout/$', logout_),
-     (r'^analyze/$',analyze_achievements),
+     url(r'^login/', login_, name = 'login'),
+     url(r'^logout/$', logout_, name = 'logout'),
+     url(r'^analyze/$',analyze_achievements, name = 'analyze'),
      url(r'^about/$', 'views.view', {'template': 'about.html'}, name = 'about'),
      url(r'^home/$', 'views.view', {'template' : 'home.html'}, name = 'home'),
 	 url(r'^profile/$', 'views.view', {'template' : 'profile.html'}, name = 'profile'),
